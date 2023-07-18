3f057b4bac9bfdd6b9e6a6e5f91e98190f85f994
andrelimao <andreluismlima@hotmail.com>
2023-07-18 19:17:10 -0300

    Update Conta.java

--- src/main/java/br/com/banco/model/Conta.java
+++ src/main/java/br/com/banco/model/Conta.java
@@ -1,19 +1,29 @@
 package br.com.banco.model;
-import javax.persistence.*;
 
-import lombok.Setter;
-import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 @Entity
 @NoArgsConstructor
+@EqualsAndHashCode(of = "conta_id")
+@ToString
 public class Conta {
-	@Id
-	@GeneratedValue
-	@Getter private int conta_id;
-	
-	@Getter @Setter private String nome_responsavel;
-	public Conta(String nome_responsavel) {
-		this.nome_responsavel = nome_responsavel;
-	
-}
+    @Id
+    @GeneratedValue
+    @Getter
+    private int conta_id;
+
+    @Getter @Setter
+    private String nome_responsavel;
+
+    public Conta(String nome_responsavel) {
+        this.nome_responsavel = nome_responsavel;
+    }
 }
