b88a38d6ec90da6991b2f439a5e1104e85ec415d
andrelimao <andreluismlima@hotmail.com>
2023-07-18 19:19:28 -0300

    Update Transferencia.java

--- src/main/java/br/com/banco/model/Transferencia.java
+++ src/main/java/br/com/banco/model/Transferencia.java
@@ -1,26 +1,38 @@
 package br.com.banco.model;
-import javax.persistence.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.sql.Timestamp;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
+
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @NoArgsConstructor
-@hashCode(of = "id")
+@EqualsAndHashCode(of = "id")
 @ToString
 @Entity
 public class Transferencia {
-	 @Id
-	 @GeneratedValue
-	private Long id;
-	@Getter @Setter private Timestamp data_transferencia;
-	@Getter @Setter private float valor;
-	@Getter @Setter private String tipo;
-	@Getter Setter private String nome_operador;
-	@Getter @Setter private int conta_id;
+    @Id
+    @GeneratedValue
+    private Long id;
 
+    @Getter @Setter
+    private Timestamp data_transferencia;
+
+    @Getter @Setter
+    private float valor;
+
+    @Getter @Setter
+    private String tipo;
+
+    @Getter @Setter
+    private String nome_operador;
+
+    @Getter @Setter
+    private int conta_id;
 }
