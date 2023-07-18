ce931925fac3d9592958174bd764ccc6ce14a3e6
andrelimao <andreluismlima@hotmail.com>
2023-07-18 19:29:13 -0300

    Atualiza TransferenciaController.java

--- src/main/java/br/com/banco/controllers/TransferenciaController.java
+++ src/main/java/br/com/banco/controllers/TransferenciaController.java
@@ -1,9 +1,13 @@
 package br.com.banco.controllers;
 import javax.persistence.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping('transferencias')
 public class TransferenciaController {
+	@Autowired
+	private TrasnferenciaRepository tr;
 	
 @GetMapping
 public List<Transferencia> mostraTransferencias{
